2b620feefc049e9a0a20fa7455e8ba645da43cc2
Logan Rakai <lmrakai@gmail.com>
2018-04-14 00:31:19 -0600

    consistent quoting

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,11 +1,11 @@
-import production from "env/production"
-import development from "env/development"
+import production from './env/production'
+import development from './env/development'
 
 const configs = {
-    "development": development,
-    "production": production
+    'development': development,
+    'production': production
 }
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 
 export default configs[env]
