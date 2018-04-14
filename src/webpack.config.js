b0807ff6e894488f8df24173f27381f11acfb42f
Logan Rakai <lmrakai@gmail.com>
2018-04-14 00:37:08 -0600

    Set correct js path

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
             {
                 loader: "babel-loader",
 
-                // Skip any files outside of your project's js directory
+                // Omit any files outside of the project's js directory
                 include: [
-                    path.resolve(__dirname, 'src/frontend'),
+                    path.resolve(__dirname, 'app/assets/js'),
                 ],
 
                 // Only load .js files through Babel
