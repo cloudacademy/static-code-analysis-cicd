811c8eb515f1f260c3a13be4043cae8eb5d359a5
Logan Rakai <lmrakai@gmail.com>
2018-04-14 00:45:37 -0600

    webpack v2

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: {
         app: path.resolve(__dirname, 'app/assets/js/index.js'),
     },
     module: {
-        loaders: [
+        rules: [
             {
                 loader: "babel-loader",
 
