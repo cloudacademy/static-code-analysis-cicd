5ed1c34045868f456e42daedfd30904084f2a497
Logan Rakai <lmrakai@gmail.com>
2018-04-14 13:52:06 -0600

    Add message logic

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,21 @@
 import $ from 'jquery';
 
-$('#message').html('Hello, World!');
+let pending = false;
+
+$('#get-message').on('click', () => {
+    if (pending === false) {
+        pending = true;
+        let input = $('input[type=text][name=name]');
+        let name = input.val();
+        input.val('');
+        $.ajax({
+            type: 'POST',
+            contentType: "text/plain; charset=utf-8",
+            data: name,
+            url: '/api',
+            success: message => $('#message').html(message),
+            error: response => conosle.log(response)
+        }).done(() => pending = false);
+    }
+});
+
