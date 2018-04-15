0d0b33774b2d737c4247a42e06d9305f3e311fa4
Logan Rakai <lmrakai@gmail.com>
2018-04-15 02:15:34 -0600

    Use AfterInstall hook to access copied files

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-test -d /home/ec2-user/app || (mkdir /home/ec2-user/app)
-rm -rf /home/ec2-user/app/*
-cp -R . /home/ec2-user/app
 cd /home/ec2-user/app
 cp -R prod_node_modules/. node_modules
